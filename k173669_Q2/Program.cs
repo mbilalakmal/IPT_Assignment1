1ec2b34f6385a5073f336329b52fea3dc1eccae9
mbilalakmal <37498621+mbilalakmal@users.noreply.github.com>
2021-02-23 13:00:57 +0500

    Added Q2 with NuGet package HTML Agility Pack for web scraping.

---
+++ k173669_Q2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace k173669_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
