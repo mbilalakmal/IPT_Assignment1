d6d43e40541bbcb9705098694ddf534cda5b75f8
mbilalakmal <37498621+mbilalakmal@users.noreply.github.com>
2021-03-11 01:29:13 +0500

    Added Blazor WASM app for Q4

---
+++ k173669_Q4/Models/Scrips.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace k173669_Q4.Models
+{
+    public class Scrips
+    {
+        public string Category { get; set; }
+        public string Scrip { get; set; }
+        public double Price { get; set; }
+    }
+}
